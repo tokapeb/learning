dafc2225cebe7fac448ab5a7ca3d9cdfe29a1974
Lőkös Kornél <Lks Kornl>
2016-05-23 22:26:44 +0200

    Add first react component.

---
+++ react/public/app.jsx
@@ -0,0 +1,15 @@
+var Greeter = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    );
+  }
+
+});
+
+ReactDOM.render(
+  <Greeter/>,
+  document.getElementById('app')
+);
