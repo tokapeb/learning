b9724743e95d733b916a09b300fa13625447c784
Lőkös Kornél <Lks Kornl>
2016-05-24 09:06:22 +0200

    New functions in app.

--- react/public/app.jsx
+++ react/public/app.jsx
@@ -1,15 +1,51 @@
 var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      msg: 'This is the Default message.'
+    }
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
+  },
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    }
+  },
   render: function () {
+    var name = this.state.name;
+    var msg = this.props.msg;
     return (
       <div>
-        <h1>Hello React!</h1>
+        <h1>Hello {name}!</h1>
+        <p>{msg}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" />
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
 
 });
 
+var name = "Kornel";
+var msg = "This is the dynamic message, yeah!";
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={name} msg={msg}/>,
   document.getElementById('app')
 );
