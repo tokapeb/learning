a6027655c4568917d32caa8d0c1ac20f1767666b
Lőkös Kornél <Lks Kornl>
2016-09-19 07:53:35 +0200

    Using arrow functions.

--- reactWeather/app/components/WeatherMessage.jsx
+++ reactWeather/app/components/WeatherMessage.jsx
@@ -1,15 +1,13 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function() {
-    var location = this.props.location;
-    var temp = this.props.temp;
-    return (
-      <div>
-        <p>The temperature is {temp} degrees at {location}.</p>
-      </div>
-    );
-  }
-});
+var WeatherMessage = (props) => {
+  var location = props.location;
+  var temp = props.temp;
+  return (
+    <div>
+      <p>The temperature is {temp} degrees at {location}. Great!</p>
+    </div>
+  );
+}
 
 module.exports = WeatherMessage;
